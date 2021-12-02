8ce7da518460b38f0e209df927cb03556c107244
Khadeeja59 <kkausar59@gmail.com>
2021-12-02 23:14:36 +0000

    Refactoring to include function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,21 @@
 const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    callback(error, null);
-  }
-  if (response && response.statusCode !== 200) {
-    error = response.statusCode;
-    callback(error, null);  
-  }
-  const data = JSON.parse(body);
-  if (!data.length) {
-    error = 'The breed queried is not found!';
-    callback(error , null);
-  }
-  else {
-    description = (data[0].description);
-    callback(null , description);
-  }
-});
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    if (response && response.statusCode !== 200) {
+      error = response.statusCode;
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (!data.length) {
+      error = 'The breed queried is not found!';
+      callback(error , null);
+    } else {
+      let description = (data[0].description);
+      callback(null , description);
+    }
+  });
 };
 module.exports = { fetchBreedDescription };
